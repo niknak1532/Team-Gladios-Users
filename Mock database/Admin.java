34d6b50f01d37929994e877ea64ef1008aa20e88
Nkosenhle N. Ncube <ncubenkosenhle@gmail.com>
2017-03-22 09:47:42 +0200

    Add files via upload

---
+++ Mock database/Admin.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Admin extends user
+{
+	ArrayList<user> adminList;
+	private boolean grant_admin_right;
+	public Admin()
+	{
+		grant_admin_right=false;
+	}
+	public Admin(boolean adminRight)
+	{
+		grant_admin_right=adminRight;
+	}
+	public void addUser(user obj)
+	{
+		if(adminList.indexOf(obj)==-1)
+			adminList.add(obj);
+	}
+	public void removeUser(user obj)
+	{
+		adminList.remove(obj);
+	}
+	public void grantAdminRight(user obj)
+	{
+		if(adminList.indexOf(obj)!=-1)
+			adminList.get(adminList.indexOf(obj)).grant_admin_right=true;
+	}
+}
