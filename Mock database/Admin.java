5f4a23f47e0e550376cb93cb8e90c20a6c849495
Nkosenhle N. Ncube <ncubenkosenhle@gmail.com>
2017-03-22 16:54:05 +0200

    Update Admin.java

--- Mock database/Admin.java
+++ Mock database/Admin.java
@@ -1,29 +1,76 @@
-import java.util.*;
+import java.sql.*;
 
 public class Admin
 {
-	ArrayList<user> adminList;
+	
+	//ArrayList<user> adminList;
+	//Statement database;
 	//private boolean grant_admin_right;
-	public Admin()
+	
+	public void addUser(Statement stmt,String userID, String username,String password,String firstname,String lastname,String email,
+			   String activate,String activateKey,String resetKey,String resetDate)
 	{
-		grant_admin_right=false;
+		// add the user with given parameters
 	}
-	public Admin(boolean adminRight)
+	public void removeUser(String userID)
 	{
-		grant_admin_right=adminRight;
+		// remove the entire cell with the userID
 	}
-	public void addUser(user obj)
+	public void grantAdminRight(String userID)
 	{
-		if(adminList.indexOf(obj)==-1)
-			adminList.add(obj);
+		// use userID to update the cell with the admin confirmation
 	}
-	public void removeUser(user obj)
+	public static void main(String[] args) 
 	{
-		adminList.remove(obj);
-	}
-	public void grantAdminRight(user obj)
-	{
-		if(adminList.indexOf(obj)!=-1)
-			adminList.get(adminList.indexOf(obj)).grant_admin_right=true;
+   		Connection conn = null;
+   		Statement stmt = null;
+   		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+
+      			//STEP 3: Open a connection
+      			System.out.println("Connecting to database...");
+      			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+      			//STEP 4: Execute a query
+      			System.out.println("Creating database...");
+      			stmt = conn.createStatement();
+      
+      			String sql = "CREATE DATABASE STUDENTS";
+      			stmt.executeUpdate(sql);
+      			System.out.println("Database created successfully...");
+   		}
+		catch(SQLException se)
+		{
+      			//Handle errors for JDBC
+      			se.printStackTrace();
+   		}
+		catch(Exception e)
+		{
+      			//Handle errors for Class.forName
+      			e.printStackTrace();
+   		}
+		finally
+		{
+      			//finally block used to close resources
+      			try
+			{
+         			if(stmt!=null)
+            				stmt.close();
+      			}
+			catch(SQLException se2)
+			{
+      			}// nothing we can do
+      			try
+			{
+         			if(conn!=null)
+            				conn.close();
+      			}
+			catch(SQLException se)
+			{
+         			se.printStackTrace();
+      			}//end finally try
+   		}//end try
+   		System.out.println("Goodbye!");
 	}
 }
