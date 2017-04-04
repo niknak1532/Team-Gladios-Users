fb7d2afef8c65650047a2bcaef7b10182efe843b
Peter Soroush <peter.soroush@gmail.com>
2017-04-04 18:30:49 +0200

    Update UserInterface.java

--- TestStaff/UserInterface.java
+++ TestStaff/UserInterface.java
@@ -9,19 +9,25 @@ public class UserInterface {
 
 
     private boolean isAdmin; //!< An abbitrary variable to be checked when mocking the database. 
-    public boolean addUser(String username, String password, String email,String );
-    /*
-    *@param iD whic will 
-    *@brief assigns admin to false
-    *@return Nothing is returned 
-    */
-	public User getUser(String username);
+    public boolean registerUser(String username,String password,String firstname,String lastname,String email, int admin);
+   /*
+	* @param username Username string
+	* @param password Password string
+	* @param firstname Firstname string
+	* @param lastname Surname string
+	* @param email Email string
+	*
+	* @todo It checks if the user exists in the databse
+	*
+	* @return Returns -1 if the user does not exist on the database. It returns the user's ID if the user exists on the database
+	*/
+    public User getUser(String username);
     public boolean removeUser(String username);
     public void activateUser(String username);
     public boolean login(String username, String password);
     public String getUsername(String username);
     public boolean getActivation(String username);
-	public boolean getEmail(String username)	;
+    public boolean getEmail(String username);
     public boolean checkAdminRights(User aUser);
     protected void setUser(String username);
     public String getActivationKey();
@@ -37,20 +43,13 @@ public class UserInterface {
     *@brief assigns admin to false
     *@return Nothing is returned 
     */
-    public Database()
-    {
-        admin=false;
-    }
-    
+    public Database();
     /*
     *@param a this is variable to determine the admin privileges (only to be used during mock demo).
     *@brief Assigns the a variable to admin.
     *@return Nothing. 
     */
-    public Database(boolean a)
-    {
-        admin=a;
-    }
+    public Database(boolean a);
     
     /*
     *@param admin this is the Admin object.
